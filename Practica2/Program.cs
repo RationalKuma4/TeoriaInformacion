ec1a32c736d096e4dbca100d08636b7e8052bfa8
Anselmo Alvarado <ans.alv.46@gmail.com>
2017-08-15 23:14:11 -0500

    Avance

--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Practica2
 {
@@ -11,11 +13,45 @@ namespace Practica2
             var pathTexto = $"{folder}cooldark.txt";
             var pathTextoProcesado = $"{folder}textoProcesado.txt";
             string texto;
-            if (!File.Exists(pathTextoProcesado))
-            {
-                ProcesarTexto(pathTexto, pathTextoProcesado, out texto);
-            }
+            if (!File.Exists(pathTextoProcesado)) ProcesarTexto(pathTexto, pathTextoProcesado, out texto);
             texto = File.ReadAllText(pathTextoProcesado);
+            RegistraTransiciones(texto);
+        }
+
+        /// <summary>
+        /// Cuenta y registra las transiciones en una lista
+        /// </summary>
+        /// <param name="texto">Texto descompuesto</param>
+        private static void RegistraTransiciones(string texto)
+        {
+            var textoDescompuesto = texto.ToCharArray();
+            var transiciones = new List<TipoTransicion>();
+
+            for (var i = 0; i < textoDescompuesto.Length; i++)
+            {
+                if (i + 1 >= textoDescompuesto.Length)
+                    break;
+                var transicion = textoDescompuesto[i].ToString() + textoDescompuesto[i + 1];
+                if (transiciones.Any(t => t.Transicion.Equals(transicion)))
+                {
+                    transiciones.First(t => t.Transicion.Equals(transicion))
+                        .Cantidad++;
+                }
+                else
+                {
+                    transiciones.Add(new TipoTransicion
+                    {
+                        Transicion = transicion,
+                        Cantidad = 1
+                    });
+                }
+            }
+
+            foreach (var tipoTransicion in transiciones)
+            {
+                Console.WriteLine($"{tipoTransicion.Transicion}, {tipoTransicion.Cantidad}");
+            }
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -70,5 +106,11 @@ namespace Practica2
 
             File.WriteAllText(pathTextoProcesado, texto);
         }
+    }
+
+    internal class TipoTransicion
+    {
+        public string Transicion { get; set; }
+        public int Cantidad { get; set; } = 0;
     }
 }
